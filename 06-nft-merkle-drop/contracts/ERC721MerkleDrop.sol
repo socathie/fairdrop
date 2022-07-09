ef48b6f0eb1381ff5e98872800fb754c1e1b1f38
Cathie So <socathie@gmail.com>
2022-07-09 18:05:31 +0800

    Initial attempt to integrate Harmony's vrf into OpenZeppelin's NFT Merkle Drop TODO: 1. unclear security after random function change: (vrf%100)+1 will return the same number for transactions that happen in the same block, so the mint function can only be called once a block - to solve this, doing (h(vrf, address)%100+1)) instead 2. If we have limited, say 100, tokens (tokenId 1-100), getting (h %100)+1  will easily have collision with minted tokens

--- 06-nft-merkle-drop/contracts/ERC721MerkleDrop.sol
+++ 06-nft-merkle-drop/contracts/ERC721MerkleDrop.sol
@@ -5,30 +5,57 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract ERC721MerkleDrop is ERC721 {
-    bytes32 immutable public root;
+    bytes32 public immutable root;
 
-    constructor(string memory name, string memory symbol, bytes32 merkleroot)
-    ERC721(name, symbol)
-    {
+    constructor(
+        string memory name,
+        string memory symbol,
+        bytes32 merkleroot
+    ) ERC721(name, symbol) {
         root = merkleroot;
     }
 
-    function redeem(address account, uint256 tokenId, bytes32[] calldata proof)
-    external
-    {
-        require(_verify(_leaf(account, tokenId), proof), "Invalid merkle proof");
-        _safeMint(account, tokenId);
+    function redeem(
+        address account,
+        uint256 tokenId,
+        bytes32[] calldata proof
+    ) external {
+        require(
+            _verify(_leaf(account, tokenId), proof),
+            "Invalid merkle proof"
+        );
+        _safeMint(account, _randomTokenId(account));
     }
 
     function _leaf(address account, uint256 tokenId)
-    internal pure returns (bytes32)
+        internal
+        pure
+        returns (bytes32)
     {
         return keccak256(abi.encodePacked(tokenId, account));
     }
 
     function _verify(bytes32 leaf, bytes32[] memory proof)
-    internal view returns (bool)
+        internal
+        view
+        returns (bool)
     {
         return MerkleProof.verify(proof, root, leaf);
+    }
+
+    function _vrf() internal view returns (bytes32 result) {
+        uint256[1] memory bn;
+        bn[0] = block.number;
+        assembly {
+            let memPtr := mload(0x40)
+            if iszero(staticcall(not(0), 0xff, bn, 0x20, memPtr, 0x20)) {
+                invalid()
+            }
+            result := mload(memPtr)
+        }
+    }
+
+    function _randomTokenId(address account) internal view returns (uint256 tokenId) {
+        tokenId = (uint256(keccak256(abi.encodePacked(uint256(_vrf()),account))) % 100)  + 1;
     }
 }
--- 06-nft-merkle-drop/hardhat.config.js
+++ 06-nft-merkle-drop/hardhat.config.js
@@ -2,6 +2,12 @@ require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('hardhat-gas-reporter');
 
+// Replace this private key with your Harmony account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const HARMONY_PRIVATE_KEY = "insert private key here";
+
 const settings = {
   optimizer: {
     enabled: true,
@@ -15,8 +21,8 @@ const settings = {
 module.exports = {
   solidity: {
     compilers: [
-      { version: '0.8.4',  settings },
-      { version: '0.7.6',  settings },
+      { version: '0.8.4', settings },
+      { version: '0.7.6', settings },
       { version: '0.6.12', settings },
       { version: '0.5.16', settings },
     ],
@@ -26,4 +32,34 @@ module.exports = {
     coinmarketcap: process.env.COINMARKETCAP,
     gasPrice: 200,
   },
+  networks: {
+    hardhat: {
+      gas: 100000000,
+      blockGasLimit: 0x1fffffffffffff,
+      forking: {
+        enabled: false,
+        url: "https://api.s0.ps.hmny.io/"
+      }
+    },
+    devnet: {
+      url: "https://api.s0.ps.hmny.io/",
+      chainId: 1666900000,
+      accounts: [`${HARMONY_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: "https://api.s0.t.hmny.io",
+      chainId: 1666600000,
+      accounts: [`${HARMONY_PRIVATE_KEY}`]
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    deploy: "deploy",
+    deployments: "deployments",
+  },
+  mocha: {
+    timeout: 1000000
+  }
 };
